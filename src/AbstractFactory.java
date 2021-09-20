40b0116b33370f05b372ead42ae0383baced4cbb
matheeshan-sivalingam <75102169+matheeshan-sivalingam@users.noreply.github.com>
2021-09-19 23:04:45 -0400

    Delete src directory

--- src/AbstractFactory.java
+++
@@ -1,8 +0,0 @@
-//Name: Matheeshan Sivalingam
-//Date: 19/09/2021
-//Project: Assignment2.java
-
-public abstract class AbstractFactory {
-    //Create abstract class to get factories for desktop and laptop objects
-    abstract Product getProduct(String productType, String name, Double price);
-}
--- src/Product.java
+++
@@ -1,6 +0,0 @@
-public interface Product {
-    //Create an abstract method for product objects to get name
-    public abstract String name();
-    //Create an abstract method for product objects to get price
-    public abstract void getPrice();
-}
